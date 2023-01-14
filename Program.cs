b25e2802202cbb53071dc922419cfdae0f5a9b3c
5541 <5541@DESKTOP-2R6N1UC>
2023-01-14 18:16:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace deliverable1_helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello World!";
+            Console.WriteLine("Solution (2): " + stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + username);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+        }
+    }
+}
